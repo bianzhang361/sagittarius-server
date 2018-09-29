16a9a53ac80c91f9b10f21abf565846c303f3cb7
bianzhang361 <bianzhang361@163.com>
2018-09-29 21:39:47 +0800

    Initial commit

---
+++ src/main/java/com/zyj/sagittarius/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.zyj.sagittarius;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SagittariusApplication.class);
+	}
+
+}
